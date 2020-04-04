f924b1d736a9164f882b81962c08cb92e3e4e33a
Calinnor <frederic.hautson@gmail.com>
2020-04-04 18:17:14 +0200

    All is ok. Favorite list is correctly displayed, neighbours, favorites or not are correctly removed from each lists. The star is grey or gold...not black !!!

---
+++ app/src/main/java/com/openclassrooms/entrevoisins/ui/neighbour_list/ListNeighbourPagerAdapter.java
@@ -0,0 +1,39 @@
+package com.openclassrooms.entrevoisins.ui.neighbour_list;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+
+public class ListNeighbourPagerAdapter extends FragmentPagerAdapter {
+
+    public ListNeighbourPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    /**
+     * getItem is called to instantiate the fragment for the given page.
+     * @param position
+     * @return
+     */
+    @Override
+    public Fragment getItem(int position) {
+        switch (position) {
+            case 0:
+                return NeighbourFragment.newInstance();
+            case 1:
+                return FavoriteNeighbourFragment.newInstance();
+            default:
+                return null;
+        }
+    }
+
+    /**
+     * get the number of pages
+     * @return
+     */
+    @Override
+    public int getCount() {
+        return 2;
+    }
+}
